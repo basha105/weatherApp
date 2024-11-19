8807acbcd3a60baceb219f4289cc95d4a58caec8
basha105 <brentbasha17@gmail.com>
2024-11-19 13:21:20 -0800

    Fetching weather data is functional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+async function getWeather() {
+    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Barbados?unitGroup=metric&key=EKEGPD87NCELHDQ85EFL3CLDA&contentType=json", {mode: 'cors'});
+    const weatherData = await response.json();
+    console.log(weatherData);
+
+    const today = weatherData.days[0];
+    
+    document.getElementById("location").textContent = weatherData.resolvedAddress;
+    document.getElementById("date").textContent = today.datetime;
+    document.getElementById("conditions").textContent = today.conditions;
+    document.getElementById("temperature").textContent = `${today.temp} °C`;
+}    
+
+getWeather();
+
+
+
