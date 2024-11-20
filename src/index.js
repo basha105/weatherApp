023db5693ed18a1bc1eedf64a8024a1b63088213
basha105 <brentbasha17@gmail.com>
2024-11-19 20:55:04 -0800

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./style.css";
 
+const days = ['Sunday, Monday, Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 async function getWeather(city) {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=EKEGPD87NCELHDQ85EFL3CLDA&contentType=json`, {mode: 'cors'});
     const weatherData = await response.json();
@@ -12,29 +14,28 @@ async function extractInfo(data) {
     let today = data.days[0];
     let temp = today.temp;
     let feelsLike = today.feelslike;
-    let desc = today.description
+    let conditionsToday = today.description
+    let conditionsGeneral = data.conditions;
     let min = today.tempmin;
     let max = today.tempmax;
+    let icon = today.icon;
 
-    return {location, today, temp, feelsLike, desc, min, max}
+    return {location, today, temp, feelsLike, conditionsToday, conditionsGeneral, min, max, icon}
 }
-
-/* async function main() {
-    let rawData = await getWeather();
-    let filteredData = await extractInfo(rawData);
-    console.log(`In ${filteredData.location}, today, ${filteredData.today.datetime}, it's currently ${filteredData.temp} °C, and feels like ${filteredData.feelsLike}.
-    ${filteredData.desc} We'll see a low of ${filteredData.min} and a high of ${filteredData.max} in the warmest hours of the day.`);
-} */
 
 async function computeSearch() {
     let search = document.getElementById("search").value;
     let rawData = await getWeather(search);
     let filteredData = await extractInfo(rawData);
 
-    document.getElementById("location").textContent = filteredData.location;
+    let dateData = new Date(`${filteredData.today.datetime}`);
+    let formattedDate = dateData.toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: '2-digit'});
+
+    document.getElementById("location").textContent = `${filteredData.location} on ${formattedTime}`;
+    document.getElementById("temp").textContent = `${filteredData.temp} °C`;
     document.getElementById("date").textContent = filteredData.today.datetime;
     document.getElementById("conditions").textContent = filteredData.desc;
-    document.getElementById("temperature").textContent = filteredData.temp;
+    
 }
 
 document.getElementById("enter").addEventListener("click", computeSearch);
