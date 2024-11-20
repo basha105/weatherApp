6c9f4c38c97ae315f2e5c8e8da47b3ab294e5092
basha105 <brentbasha17@gmail.com>
2024-11-19 18:12:14 -0800

    Search bar linked with display and API functionality! All is tied together

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
-async function getWeather() {
-    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Vancouver?unitGroup=metric&key=EKEGPD87NCELHDQ85EFL3CLDA&contentType=json", {mode: 'cors'});
+async function getWeather(city) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=EKEGPD87NCELHDQ85EFL3CLDA&contentType=json`, {mode: 'cors'});
     const weatherData = await response.json();
     console.log(weatherData);
     return weatherData;
@@ -19,14 +19,25 @@ async function extractInfo(data) {
     return {location, today, temp, feelsLike, desc, min, max}
 }
 
-async function main() {
+/* async function main() {
     let rawData = await getWeather();
     let filteredData = await extractInfo(rawData);
     console.log(`In ${filteredData.location}, today, ${filteredData.today.datetime}, it's currently ${filteredData.temp} °C, and feels like ${filteredData.feelsLike}.
     ${filteredData.desc} We'll see a low of ${filteredData.min} and a high of ${filteredData.max} in the warmest hours of the day.`);
+} */
+
+async function computeSearch() {
+    let search = document.getElementById("search").value;
+    let rawData = await getWeather(search);
+    let filteredData = await extractInfo(rawData);
+
+    document.getElementById("location").textContent = filteredData.location;
+    document.getElementById("date").textContent = filteredData.today.datetime;
+    document.getElementById("conditions").textContent = filteredData.desc;
+    document.getElementById("temperature").textContent = filteredData.temp;
 }
 
-main();
+document.getElementById("enter").addEventListener("click", computeSearch);
 
 
 
